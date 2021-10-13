8a0fc6b3e27380c054f357056b0452b2d0abb34e
lucamagenes <lucamgns33@gmail.com>
2021-10-13 15:20:44 +0200

    struttura si base html css js

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+//Scrivi un programma che esegua un ciclo da 1 a 100
+
+
+
+/*
+
+-seleziono la classe della lista non ordinata
+
+-creo loop di elementi da 1 a 100
+
+*/
+
+
+
+//stile differente per i valori multipli di 3
+
+/*
+
+-seleziono solo i multipli di 3
+
+-assegno a questi elementi uno stile differente
+
+*/
+
+
+//stile differente per i valori multipli di 5
+
+/*
+
+-seleziono solo i multipli di 5
+
+-assegno a questi elementi uno stile differente
+
+*/
+
+
+//stile differente per i valori multipli di 3 e di 5
+
+/*
+
+-seleziono i valori che sono allo stesso tempo multipli di 3 e 5
+
+-assegno a questi elementi uno stile differente
+
+*/
